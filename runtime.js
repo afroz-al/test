6f0f0221f2b0758bd37dbe7497981ba27d7b6ecb
afroz-al <71060092+afroz-al@users.noreply.github.com>
2023-01-10 08:58:11 -0800

    Delete runtime.js

--- runtime.js
+++
@@ -1,6 +0,0 @@
-function onMessageSendHandler(event) {
-  event.completed({ allowEvent: false, errorMessage: "testing again" });
-}
-
-//Office.actions.associate("checkSignature", onMessageSendHandler);
-Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
